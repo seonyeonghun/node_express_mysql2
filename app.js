bef263aeab907926602d10ed123e44c471e5bafe
seonyeonghun <sprax@naver.com>
2023-10-05 17:40:33 +0900

    mysql dateString 설정 추가

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const conn = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWD,
   database: process.env.DB_SCHEME,
+  dateStrings: "date",
 });
 console.log("connected to mysql db");
 
@@ -39,7 +40,7 @@ app.post("/contactProc", (req, res) => {
   const user_email = req.body.email;
   const user_memo = req.body.memo;
   //console.log(user_name);
-  let sql = `insert into contact(name,phone,email,memo,regdate) values ('${user_name}','${user_phone}', '${user_email}', '${user_memo}', now())`;
+  let sql = `insert into contact(name,phone,email,memo,regdate) values ('${user_name}','${user_phone}', '${user_email}', '${user_memo}', DATE_FORMAT(now(), '%y/%m/%d'))`;
   conn.query(sql, (error, result) => {
     if (error) throw error;
     console.log("1 rows inserted");
@@ -56,11 +57,11 @@ app.post("/contactProc", (req, res) => {
 
 // 관리자용 | 외부노출 x | 문의내용 확인
 app.get("/contactList", (req, res) => {
-  let sql = `select * from contact`;
+  let sql = `select * from contact order by idx desc`;
   conn.query(sql, (error, result, fields) => {
     if (error) throw error;
-    console.log(result);
-    res.render("contactList",{lists: result})
+    //console.log(result);
+    res.render("contactList", { lists: result });
   });
 });
 
